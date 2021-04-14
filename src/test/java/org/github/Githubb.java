402df4033a0b154ce9ce57c02771f782dc47a2ca
Mishael1989 <89michaelraj@gmail.com>
2021-04-14 18:03:03 +0530

    providing access

---
+++ src/test/java/org/github/Githubb.java
@@ -0,0 +1,16 @@
+package org.github;
+
+public class Githubb {
+	
+	private void method1() {
+		System.out.println("Method 1");
+	}
+		
+	private void method2() {
+		System.out.println("method 2");
+	}
+	
+	private void method3() {
+		System.out.println("method 3");
+	}
+}
